c1b4b37d02c654e59a52448737ecd4287884190b
Franklin Chou <franklin.chou@ferc.gov>
2018-07-10 11:28:04 -0400

    Chapter 1 of training day

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,11 @@
-(ns training-day)
+(ns training-day) ;declares a namespace ns called training day but hyphen instead of underscore.underscore for file nam
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x ))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
